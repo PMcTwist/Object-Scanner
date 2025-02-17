5f9a77e2a5c38db33c898011249a8ecfa17a887c
P@ <patmaynard452@hotmail.com>
2025-02-16 22:37:37 -0500

    - Had a hell of a time with real time system interrupts. Needed to be able to send a stop command while running nested loops. - Pulled the first part of the real-time loop out into a checkForUpdates function that can be called at the beginning of each iteration to let the HMI maintain control while making the commands serially. - Setup a stopFlag variable to monitor if a stop command has been issued and gave steps to bring the system to a clean stop state. - Setup the full data array. - Setup the end of the run function to deconsruct the array for serial transmission to the API call on the python worker thread. - Added in an ass-load of calculations for rotational movements with relation to the platform to measure the Y axis distance. And added in a rough guess at the 8mm rod calculation with relation to a 1.8deg step for the Z axis. - Fixed a ton of variable names that had -'s in them...because that was a n00b mistake.

--- Arduino Code/Controller_Code/Capstone_trail_combined.ino
+++ Arduino Code/Controller_Code/Capstone_trail_combined.ino
@@ -12,17 +12,18 @@ int16_t tfAddr = TFL_DEF_ADR;  // use this default I2C address
 
 // ========== Platform global Variables =====================
 // This section is for the Y-axis
-const float y-StepAngle = 1.8;
+const float y_StepAngle = 1.8;
 const float platformRadius = 100; // in mm
 
 // Platform Calculation Functions
-const float stepsPerRev = 360/y-StepAngle;
+const float stepsPerRev = 360/y_StepAngle;
 const float platformCircumference = 2.0 * 3.1415926535 * platformRadius;
-const float y-DistancePerStep = platformCircumference / stepsPerRev;
+const float y_DistancePerStep = platformCircumference / stepsPerRev;
 
 // ========= Threaded Rod Global Variables ===================
 // This section is for the Z-axis
-const float z-StepAngle = 1.8;
+const float rodPitch = 2; // in mm
+const float z_DistancePerStep = rodPitch / stepsPerRev;
 
 
 
@@ -48,11 +49,12 @@ AccelStepper stepperZ(motorZInterfaceType, Z_STEP_PIN, Z_DIR_PIN);
 
 // ========= Setup the state of the machine ========================
 bool running = false;         // flag for state
+bool stopFlag = true;         // flag for system interupt
 
 // Variable to hold travel distances
-const float x_dist = 0.0;
-const float y-axis_total_distance = 0.0;
-const float z-axis_total_distance = 0.0;
+float x_dist = 0.0;
+float y_axis_total_distance = 0.0;
+float z_axis_total_distance = 0.0;
 
 // Array of data to send
 int dataArray[3];
@@ -72,60 +74,97 @@ void setup() {
 
   // Set intial flag for running state
   running = false;
+  stopFlag = false;
 
 }
 
 // Runtime Function
 void loop() {
-
-  if (Serial.available() > 0) {      // Check if there's serial input
-    char inputChar = Serial.read();  // Read the input character from the UI button
-    if (inputChar == '1') {          // Start scanning
-      running = true;
-      Serial.println("Scanning Started");
-    } else if (inputChar == '0') {   // Stop scanning
-      running = false;
-      Serial.println("Scanning Stopped");
-    }
-  }
-
+  // Check for initial start signal
+  checkForUpdates();
 
   // Basic condition for running state (scanning)
-  if (running == true) {
+  if (running) {
     // Setup conditional for reading LiDAR data
     if (tflI2C.getData(tfDist, tfAddr))
     {
       // For every step in 360deg of Y motor scan
       // Then move up 1 step on the Z motor 
       for (int z = 0; z < 200; z++){
-        // Make Z Step
-        stepperZ.move(1);
-        stepperZ.runToPosition();
+        // Check for Serial commands
+        checkForUpdates();
 
-        z-axis_total_distance += z-DistancePerStep;
+        if (!stopFlag) {
+          // Make Z Step
+          stepperZ.move(1);
+          stepperZ.run();
 
-        for (int y = 0; y < 200; y++) {
-          // Make Y Step
-          stepperY.move(1);
-          stepperY.runToPosition();
+          // Update the total Z distance
+          z_axis_total_distance += z_DistancePerStep;
 
-          // update the total Y distance
-          y-axis_total_distance += y-DistancePerStep;
+          for (int y = 0; y < 200; y++) {
+            // Check for Serial Commands
+            checkForUpdates();
 
-          // Meansure X Distance from Sensor
-          x_dist = tfDist * 10;
+            if (!stopFlag) {
+              // Make Y Step
+              stepperY.move(1);
+              stepperY.run();
+
+              // update the total Y distance
+              y_axis_total_distance += y_DistancePerStep;
+
+              // Meansure X Distance from Sensor and convert to mm
+              x_dist = tfDist * 10;
+            } if (stopFlag) {
+              break; // Kills inner loop on stop code
+            }
+          }
+        } if (stopFlag) {
+          break; // Kills the main for loop on stop code
         }
 
         // Update the dataArray to hold the X, Y, Z values to send to HMI
-        dataArray = {x_dist, y-axis_total_distance, z-axis_total_distance};
-        Serial.println(dataArray);
+        dataArray[0] = x_dist;
+        dataArray[1] = y_axis_total_distance; 
+        dataArray[2] = z_axis_total_distance;
+
+        // Create a string with the array values separated by commas
+        String dataToSend = String(dataArray[0]) + "," + String(dataArray[1]) + "," + String(dataArray[2]);
+
+
+        // Send the string to the Python listener
+        Serial.println(dataToSend);
       }    
     } else {
+      stepperY.stop();
+      stepperZ.stop();
       tflI2C.printStatus();
     }  
 
     // Set Delay between scans
-    delay(5000);
+    delay(500);
   }
-  
+  // Avoid breaking the communication
+  delay (50);
+}
+
+void checkForUpdates() {
+  if (Serial.available() > 0) {      // Check if there's serial input
+    char inputChar = Serial.read();  // Read the input character from the UI button
+    Serial.println(inputChar);
+    if (inputChar == '1') {          // Start scanning
+      if (!running){
+        running = true;
+        stopFlag = false;
+      Serial.println("Scanning...");
+      }
+    } else if (inputChar == '0') {   // Stop scanning
+      if (running) {
+        running = false;
+        stopFlag = true;
+        Serial.println("Scanning Stopped");
+      }
+    }
+  }
 }
